d15030fe9a217d3d04e1cb6ab2b5ac43b3aca7a5
Gokhan <engokhangok@gmail.com>
2023-08-12 09:39:42 +0300

    Add project files.

---
+++ Services/Order/Core/CasgemMicroservice.Services.Order.Core.Application/Mappings/AdressProfile.cs
@@ -0,0 +1,21 @@
+﻿using CasgemMicroservice.Services.Order.Core.Application.Dtos.AdressDtos;
+using CasgemMicroservice.Services.Order.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace CasgemMicroservice.Services.Order.Core.Application.Mappings
+{
+	public class AdressProfile : Profile
+	{
+		public AdressProfile()
+		{
+			CreateMap<ResultAdressDto, Adress>().ReverseMap();
+			CreateMap<CreateAdressDto, Adress>().ReverseMap();
+			CreateMap<UpdateAdressDto, Adress>().ReverseMap();
+		}
+	}
+}
